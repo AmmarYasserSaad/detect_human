d8dd6e12954dcaefacf609d8bb38c88dd40d5bb4
AmmarYasserSaad <ammaryasser2413@gmail.com>
2022-09-09 00:29:56 +0200

    add comments

--- detection.py
+++ detection.py
@@ -1,15 +1,19 @@
 import cv2
-from matplotlib import image
-import numpy as np
 def det_hum(image_path):
+    # define hog algorithm
     hog=cv2.HOGDescriptor()
+    # use petrained model to detect humen
     hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
     image=cv2.imread(image_path)
+    # detect bound of rectangle
     bound,wights=hog.detectMultiScale(image,winStride=(4,4), padding=(4,4), scale=1.05)
     for (x,y,w,h) in bound:
         cv2.rectangle(image,(x,y),(x+w,y+h),(0,0,255),4)
+    # get path size
     size=len(image_path)
+    # modify image path
     new_path=image_path[:size-4]
     new_path=new_path+'_detct.jpg'
+    # save detect image in new path
     cv2.imwrite(new_path,image)
     return new_path
--- gui.py
+++ gui.py
@@ -3,7 +3,7 @@ from PIL import Image
 from detection import det_hum
 import os
 import io
-
+# define shape of gui
 layout=[
     [sg.Text('choose image'),sg.Input(),sg.FileBrowse(key="-file-")],
     [sg.Button('Submit'),sg.Button("Detect")],
@@ -17,14 +17,17 @@ while True:
         break
     if event =='Exit':
         break
+    #show image if press submit
     if event == 'Submit':
         print(values)
         if os.path.exists(values["-file-"]):
+            # change image to png
             image= Image.open(values["-file-"])
             image.thumbnail((400, 400))
             bio = io.BytesIO()
             image.save(bio, format="PNG")
             window["-image-"].update(data=bio.getvalue())
+    #show image after detect humen if press detect
     if event == 'Detect':
         print(values)
         image=det_hum(values['-file-'])
